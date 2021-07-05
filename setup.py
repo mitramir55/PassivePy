925ca6127de7473e21bf83ab963d8c54db681e2d
53291220 <mitra.mirshafiee@gmail.com>
2021-07-05 20:43:48 +0430

    version 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="PassivePy",
-    version="0.0.16",
+    version="0.1.0",
     author="Mitra Mirshafiee",
     author_email="mitra.mirshafiee@gmail.com",
     description="A package for processing large corpora and detecting passive voice.",
@@ -27,12 +27,13 @@ setup(
         "Operating System :: OS Independent",
         
     ],
-    keywords='passive voice, text analysis, spacy, dependency parsing, part of speech tagging',  
+    keywords='passive voice, text analysis, spacy, dependency parsing, part of speech tagging',
+    package_dir={'': 'PassivePyCode'},
+    packages=find_packages('PassivePyCode'),  
     python_requires=">=3.6",
 
     install_requires=['termcolor', 'tqdm', 'spacy>=3.0.0'],
     dependency_links=[
-        
         'https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg-3.0.0.tar.gz#egg=en_core_web_lg'
     ]
 
