64850a0a7e341d206e67cc4b584a9a435657335b
53291220 <mitra.mirshafiee@gmail.com>
2021-07-07 20:54:46 +0430

    change the link to the dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
 
     install_requires=['termcolor', 'tqdm', 'spacy>=3.0.0'],
     dependency_links=[
-        'https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg-3.0.0.tar.gz#egg=en_core_web_lg'
+        'https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.0.0/en_core_web_lg_3.0.0.tar.gz#egg=en_core_web_lg'
     ]
 
 )
