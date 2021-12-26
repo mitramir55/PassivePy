19bb3447fc3424235b5916121ec001ab241b35ea
mitramir55 <mitra.mirshafiee@yahoo.com>
2021-12-26 19:16:40 +0330

    readme setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Mitra Mirshafiee",
     author_email="mitra.mirshafiee@gmail.com",
     description="A package for processing large corpora and detecting passive voice.",
-    long_description="Our aim with this work is to create a reliable (e.g., passive voice judgments are consistent), valid (e.g., passive voice judgments are accurate), flexible (e.g., texts can be assessed at different units of analysis), replicable (e.g., the approach can be performed by a range of research teams with varying levels of computational expertise), and scalable way (e.g., small and large collections of texts can be analyzed) to capture passive voice from different corpora for social and psychological evaluations of text. To achieve these aims, we introduce PassivePy, a fully transparent and documented Python library.",
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     url="https://github.com/mitramir55/PassivePy",
     project_urls={
