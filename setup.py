1c9703726a9cd602722e677bc9c48842b7435027
mitramir55 <mitra.mirshafiee@yahoo.com>
2021-12-26 20:41:46 +0330

    version 0.2.0

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@
 from setuptools import setup, find_packages
 import pathlib
 
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
 setup(
     name="PassivePy",
-    version="0.2.0",
+    version="0.2.2",
     author="Mitra Mirshafiee",
     author_email="mitra.mirshafiee@gmail.com",
     description="A package for processing large corpora and detecting passive voice.",
-    long_description=read('README.md'),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mitramir55/PassivePy",
     project_urls={
